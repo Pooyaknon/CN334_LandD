f66261dcc6b6ad6aead41cafbe2083d29909d5fc
Pooyaknon <l.poorinut@gmail.com>
2025-05-05 15:05:26 +0700

    Update order serializers - Poo

--- order/serializers.py
+++ order/serializers.py
@@ -12,14 +12,23 @@ class DeliveryMethodSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class OrderSerializer(serializers.ModelSerializer):
-    land_name = serializers.CharField(source='land.name', read_only=True)
-    payment_method_name = serializers.CharField(source='payment_method.method_name', read_only=True)
-    delivery_method_name = serializers.CharField(source='delivery_method.method_name', read_only=True)
-    
+    land_name = serializers.CharField(source='land.title', read_only=True)
+    land_price = serializers.DecimalField(source='land.price', max_digits=12, decimal_places=2, read_only=True)
+    land_size = serializers.FloatField(source='land.size', read_only=True)
+    land_location = serializers.CharField(source='land.location', read_only=True)
+    land_promotion = serializers.CharField(source='land.promotion.name', read_only=True, default=None)
+    land_discount_percent = serializers.IntegerField(source='land.promotion.discount_percent', read_only=True, default=0)
+    land_image = serializers.SerializerMethodField()
+
+    def get_land_image(self, obj):
+        first_image = obj.land.images.first()
+        return first_image.image_url if first_image else None
+
     class Meta:
         model = Order
         fields = [
-            'id', 'user', 'land', 'land_name',
+            'id', 'user', 'land', 'land_name', 'land_price', 'land_size',
+            'land_location', 'land_promotion', 'land_discount_percent', 'land_image',
             'payment_method', 'payment_method_name',
             'delivery_method', 'delivery_method_name',
             'order_date', 'status'
